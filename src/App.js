19835751113cdf3418801ed1aec73c4a834ba779
Jobwafula <jobwafulabg@gmail.com>
2024-02-11 20:37:34 +0300

    first commmit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      Hello, Devs!
+      <p>Tempor et sunt cupidatat ut enim. Laboris laborum occaecat quis anim pariatur voluptate incididunt adipisicing. Lorem minim consequat elit irure fugiat tempor cillum elit magna. Dolore deserunt ad dolore occaecat consequat consequat laboris non tempor. Amet tempor mollit nostrud proident enim culpa aliqua.</p>
+    </div>
+  );
+}
+
+export default App
